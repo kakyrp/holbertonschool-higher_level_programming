9567af4bff99d8d6af4d8e72469b3baa6dca1eb0
kakyrp <karodri.p@gmail.com>
2020-12-08 22:34:14 +0000

    Advanced Task with ternary expresion

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,4 @@
 
 
 for i in range(122, 96, -1):
-    if((i % 2) == 0):
-        print(chr(i), end="")
-    else:
-        print(chr(i-32), end="")
+    print(chr(i if i % 2 == 0 else i-32), end="")
